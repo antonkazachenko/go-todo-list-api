4bfc00ea923081cf52a70a27857496c6b3d0ec43
creamlaflare <antonkazachenko2002@gmail.com>
2024-07-13 17:01:26 -0700

    create web-server

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/creamlaflare/go-todo-list-api
 go 1.22.2
 
 require (
+	github.com/go-chi/chi/v5 v5.1.0
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/mattn/go-sqlite3 v1.14.22
 	github.com/stretchr/testify v1.9.0
--- main.go
+++ main.go
@@ -1,1 +1,32 @@
-package go_todo_list_api
+package main
+
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	PORT := os.Getenv("TODO_PART")
+
+	if PORT == "" {
+		PORT = "7540"
+	}
+
+	fileServer := http.FileServer(http.Dir("./web"))
+	r.Get("/*", func(w http.ResponseWriter, r *http.Request) {
+		if filepath.Ext(r.URL.Path) == ".css" {
+			w.Header().Set("Content-Type", "text/css")
+		}
+		fileServer.ServeHTTP(w, r)
+	})
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", PORT), r); err != nil {
+		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
+		return
+	}
+}
