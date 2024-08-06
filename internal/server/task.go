553edb0f39e8167b41b3918df5ba7f7ad38f0c90
creamlaflare <antonkazachenko2002@gmail.com>
2024-08-06 15:55:55 -0700

    optimize server layer

--- internal/server/task.go
+++ internal/server/task.go
@@ -24,16 +24,8 @@ func NewHandlers(taskService *service.TaskService) *Handlers {
 }
 
 func (h *Handlers) HandleAddTask(res http.ResponseWriter, req *http.Request) {
-	var buf bytes.Buffer
 	var task entities.Task
-
-	_, err := buf.ReadFrom(req.Body)
-	if err != nil {
-		utils.SendErrorResponse(res, "ошибка чтения тела запроса", http.StatusBadRequest)
-		return
-	}
-
-	if err = json.Unmarshal(buf.Bytes(), &task); err != nil {
+	if err := parseRequestBody(req, &task); err != nil {
 		utils.SendErrorResponse(res, "ошибка декодирования JSON", http.StatusBadRequest)
 		return
 	}
@@ -43,29 +35,9 @@ func (h *Handlers) HandleAddTask(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	var dateInTime time.Time
-	if task.Date != "" {
-		dateInTime, err = time.Parse(service.Format, task.Date)
-		if err != nil {
-			utils.SendErrorResponse(res, "недопустимый формат date", http.StatusBadRequest)
-			return
-		}
-	} else {
-		task.Date = time.Now().Format(service.Format)
-		dateInTime = time.Now()
-	}
-
-	if time.Now().After(dateInTime) {
-		if task.Repeat == "" {
-			task.Date = time.Now().Format(service.Format)
-			dateInTime = time.Now()
-		} else {
-			task.Date, err = h.TaskService.NextDate(time.Now(), task.Date, task.Repeat)
-			if err != nil {
-				utils.SendErrorResponse(res, err.Error(), http.StatusBadRequest)
-				return
-			}
-		}
+	if err := h.validateAndUpdateDate(&task); err != nil {
+		utils.SendErrorResponse(res, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	id, err := h.TaskService.Repo.AddTask(task)
@@ -74,19 +46,7 @@ func (h *Handlers) HandleAddTask(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	resp := models.IDResponse{ID: id}
-	respBytes, err := json.Marshal(resp)
-	if err != nil {
-		http.Error(res, fmt.Sprintf("ошибка при сериализации ответа: %v", err), http.StatusBadRequest)
-		return
-	}
-	res.Header().Set("Content-Type", "application/json")
-	res.WriteHeader(http.StatusOK)
-	_, err = res.Write(respBytes)
-	if err != nil {
-		http.Error(res, fmt.Sprintf("ошибка при записи ответа: %v", err), http.StatusBadRequest)
-		return
-	}
+	sendJSONResponse(res, http.StatusOK, models.IDResponse{ID: id})
 }
 
 func (h *Handlers) HandleGetTasks(res http.ResponseWriter, req *http.Request) {
@@ -99,211 +59,96 @@ func (h *Handlers) HandleGetTasks(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	if tasks == nil {
+	if tasks == nil || len(tasks) == 0 {
 		tasks = []entities.Task{}
 	}
-	respBytes, err := json.Marshal(map[string][]entities.Task{"tasks": tasks})
-	if err != nil {
-		utils.SendErrorResponse(res, "ошибка сериализации ответа", http.StatusInternalServerError)
-		return
-	}
-	res.Header().Set("Content-Type", "application/json")
-	res.WriteHeader(http.StatusOK)
-	_, err = res.Write(respBytes)
-	if err != nil {
-		utils.SendErrorResponse(res, "ошибка записи ответа", http.StatusInternalServerError)
-		return
-	}
+
+	sendJSONResponse(res, http.StatusOK, map[string][]entities.Task{"tasks": tasks})
 }
 
 func (h *Handlers) HandleGetTask(res http.ResponseWriter, req *http.Request) {
-	id := req.URL.Query().Get("id")
+	taskID, err := parseAndValidateID(req.URL.Query().Get("id"))
+	if err != nil {
+		utils.SendErrorResponse(res, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-	id, err := parseAndValidateID(id)
-
-	task, err := h.TaskService.Repo.GetTaskByID(id)
+	task, err := h.TaskService.Repo.GetTaskByID(taskID)
 	if err != nil {
 		utils.SendErrorResponse(res, "задача с указанным id не найдена", http.StatusNotFound)
 		return
 	}
 
-	respBytes, err := json.Marshal(task)
-	if err != nil {
-		utils.SendErrorResponse(res, "ошибка сериализации ответа", http.StatusInternalServerError)
-		return
-	}
-	res.Header().Set("Content-Type", "application/json")
-	res.WriteHeader(http.StatusOK)
-	_, err = res.Write(respBytes)
-	if err != nil {
-		utils.SendErrorResponse(res, "ошибка записи ответа", http.StatusInternalServerError)
-		return
-	}
+	sendJSONResponse(res, http.StatusOK, task)
 }
 
 func (h *Handlers) HandlePutTask(res http.ResponseWriter, req *http.Request) {
 	var taskUpdates map[string]interface{}
-
-	err := json.NewDecoder(req.Body).Decode(&taskUpdates)
-	if err != nil {
+	if err := parseRequestBody(req, &taskUpdates); err != nil {
 		utils.SendErrorResponse(res, "ошибка декодирования JSON", http.StatusBadRequest)
 		return
 	}
 
-	id, ok := taskUpdates["id"].(string)
-	if !ok || id == "" {
-		utils.SendErrorResponse(res, "отсутствует обязательное поле id", http.StatusBadRequest)
-		return
-	}
-
-	_, err = strconv.ParseInt(id, 10, 64)
+	_, err := h.validateAndExtractID(taskUpdates)
 	if err != nil {
-		utils.SendErrorResponse(res, "id должен быть числом", http.StatusBadRequest)
+		utils.SendErrorResponse(res, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	_, err = h.TaskService.Repo.GetTaskByID(id)
-	if err != nil {
-		utils.SendErrorResponse(res, "задача с указанным id не найдена", http.StatusNotFound)
+	if err := h.validateTaskUpdates(taskUpdates); err != nil {
+		utils.SendErrorResponse(res, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	title, ok := taskUpdates["title"].(string)
-	if !ok || title == "" {
-		utils.SendErrorResponse(res, "отсутствует обязательное поле title", http.StatusBadRequest)
-		return
-	}
-
-	date, ok := taskUpdates["date"].(string)
-	if !ok || date == "" {
-		utils.SendErrorResponse(res, "отсутствует обязательное поле date", http.StatusBadRequest)
-		return
-	}
-
-	_, err = time.Parse("20060102", date)
-	if err != nil {
-		utils.SendErrorResponse(res, "недопустимый формат date", http.StatusBadRequest)
-		return
-	}
-
-	if repeat, ok := taskUpdates["repeat"].(string); ok {
-		if repeat != "" {
-			repeatParts := strings.SplitN(repeat, " ", 2)
-			repeatType := repeatParts[0]
-			if repeatType != "d" && repeatType != "w" && repeatType != "m" && repeatType != "y" {
-				utils.SendErrorResponse(res, "недопустимый символ", http.StatusBadRequest)
-				return
-			}
-		}
-	}
-
-	_, err = h.TaskService.Repo.UpdateTask(taskUpdates)
-	if err != nil {
+	if _, err := h.TaskService.Repo.UpdateTask(taskUpdates); err != nil {
 		utils.SendErrorResponse(res, "ошибка запроса к базе данных", http.StatusInternalServerError)
 		return
 	}
 
-	res.Header().Set("Content-Type", "application/json")
-	res.WriteHeader(http.StatusOK)
-	_, err = res.Write([]byte(`{}`))
-	if err != nil {
-		utils.SendErrorResponse(res, "ошибка записи ответа", http.StatusInternalServerError)
-		return
-	}
+	sendJSONResponse(res, http.StatusOK, nil)
 }
 
 func (h *Handlers) HandleDeleteTask(res http.ResponseWriter, req *http.Request) {
-	id := req.URL.Query().Get("id")
-
-	id, err := parseAndValidateID(id)
-
-	_, err = strconv.ParseInt(id, 10, 64)
+	taskID, err := parseAndValidateID(req.URL.Query().Get("id"))
 	if err != nil {
-		utils.SendErrorResponse(res, "id должен быть числом", http.StatusBadRequest)
+		utils.SendErrorResponse(res, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	_, err = h.TaskService.Repo.GetTaskByID(id)
-	if err != nil {
-		utils.SendErrorResponse(res, "задача с указанным id не найдена", http.StatusNotFound)
+	if err := h.deleteTaskIfExists(taskID); err != nil {
+		utils.SendErrorResponse(res, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	_, err = h.TaskService.Repo.DeleteTask(id)
-	if err != nil {
-		utils.SendErrorResponse(res, "ошибка запроса к базе данных", http.StatusInternalServerError)
-		return
-	}
-
-	res.Header().Set("Content-Type", "application/json")
-	res.WriteHeader(http.StatusOK)
-	respBytes, err := json.Marshal(struct{}{})
-	if err != nil {
-		http.Error(res, "ошибка при сериализации ответа", http.StatusInternalServerError)
-		return
-	}
-	_, err = res.Write(respBytes)
-	if err != nil {
-		utils.SendErrorResponse(res, "ошибка записи ответа", http.StatusInternalServerError)
-		return
-	}
+	sendJSONResponse(res, http.StatusOK, nil)
 }
 
 func (h *Handlers) HandleDoneTask(res http.ResponseWriter, req *http.Request) {
-	id := req.URL.Query().Get("id")
+	taskID, err := parseAndValidateID(req.URL.Query().Get("id"))
+	if err != nil {
+		utils.SendErrorResponse(res, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-	id, err := parseAndValidateID(id)
-
-	task, err := h.TaskService.Repo.GetTaskByID(id)
+	task, err := h.TaskService.Repo.GetTaskByID(taskID)
 	if err != nil {
 		utils.SendErrorResponse(res, "задача с указанным id не найдена", http.StatusNotFound)
 		return
 	}
 
 	if task.Repeat == "" {
-		_, err = h.TaskService.Repo.DeleteTask(id)
-		if err != nil {
+		if _, err := h.TaskService.Repo.DeleteTask(taskID); err != nil {
 			utils.SendErrorResponse(res, "ошибка запроса к базе данных", http.StatusInternalServerError)
 			return
 		}
 	} else {
-		parsedDate, err := time.Parse(service.Format, task.Date)
-		if err != nil {
-			utils.SendErrorResponse(res, "недопустимый формат date", http.StatusBadRequest)
-			return
-		}
-
-		if parsedDate.Format(service.Format) == time.Now().Format(service.Format) {
-			parsedDate = parsedDate.AddDate(0, 0, -1)
-			task.Date, err = h.TaskService.NextDate(parsedDate, task.Date, task.Repeat)
-		} else {
-			task.Date, err = h.TaskService.NextDate(time.Now(), task.Date, task.Repeat)
-		}
-
-		if err != nil {
-			utils.SendErrorResponse(res, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		err = h.TaskService.Repo.MarkTaskAsDone(id, task.Date)
-		if err != nil {
-			utils.SendErrorResponse(res, "ошибка запроса к базе данных", http.StatusInternalServerError)
+		if err := h.markTaskAsDone(taskID, task); err != nil {
+			utils.SendErrorResponse(res, err.Error(), http.StatusInternalServerError)
 			return
 		}
 	}
 
-	res.Header().Set("Content-Type", "application/json")
-	res.WriteHeader(http.StatusOK)
-	respBytes, err := json.Marshal(struct{}{})
-	if err != nil {
-		http.Error(res, "ошибка при сериализации ответа", http.StatusInternalServerError)
-		return
-	}
-	_, err = res.Write(respBytes)
-	if err != nil {
-		utils.SendErrorResponse(res, "ошибка записи ответа", http.StatusInternalServerError)
-		return
-	}
+	sendJSONResponse(res, http.StatusOK, nil)
 }
 
 func (h *Handlers) HandleNextDate(res http.ResponseWriter, req *http.Request) {
@@ -312,15 +157,12 @@ func (h *Handlers) HandleNextDate(res http.ResponseWriter, req *http.Request) {
 	repeat := req.URL.Query().Get("repeat")
 
 	now, err := time.Parse(service.Format, nowParam)
-
 	if err != nil {
 		http.Error(res, "Неправильный формат парамeтра now", http.StatusBadRequest)
 		return
 	}
-	var newDate string
-	if repeat != "" {
-		newDate, err = h.TaskService.NextDate(now, date, repeat)
-	}
+
+	newDate, err := h.TaskService.NextDate(now, date, repeat)
 	if err != nil {
 		http.Error(res, err.Error(), http.StatusBadRequest)
 		return
@@ -335,6 +177,29 @@ func (h *Handlers) HandleNextDate(res http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func parseRequestBody(req *http.Request, target interface{}) error {
+	var buf bytes.Buffer
+	if _, err := buf.ReadFrom(req.Body); err != nil {
+		return fmt.Errorf("ошибка чтения тела запроса")
+	}
+	return json.Unmarshal(buf.Bytes(), target)
+}
+
+func sendJSONResponse(res http.ResponseWriter, statusCode int, data interface{}) {
+	respBytes, err := json.Marshal(data)
+	if err != nil {
+		utils.SendErrorResponse(res, "ошибка при сериализации ответа", http.StatusInternalServerError)
+		return
+	}
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(statusCode)
+	_, err = res.Write(respBytes)
+	if err != nil {
+		utils.SendErrorResponse(res, "ошибка записи ответа", http.StatusInternalServerError)
+		return
+	}
+}
+
 func parseAndValidateID(idStr string) (string, error) {
 	if idStr == "" {
 		return "", fmt.Errorf("не передан идентификатор")
@@ -344,4 +209,121 @@ func parseAndValidateID(idStr string) (string, error) {
 		return "", fmt.Errorf("id должен быть числом")
 	}
 	return idStr, nil
+}
+
+func (h *Handlers) validateAndExtractID(taskUpdates map[string]interface{}) (string, error) {
+	id, ok := taskUpdates["id"].(string)
+	if !ok || id == "" {
+		return "", fmt.Errorf("отсутствует обязательное поле id")
+	}
+
+	if _, err := strconv.ParseInt(id, 10, 64); err != nil {
+		return "", fmt.Errorf("id должен быть числом")
+	}
+
+	if _, err := h.TaskService.Repo.GetTaskByID(id); err != nil {
+		return "", fmt.Errorf("задача с указанным id не найдена")
+	}
+
+	return id, nil
+}
+
+func (h *Handlers) validateTaskUpdates(taskUpdates map[string]interface{}) error {
+	date, dateOk := taskUpdates["date"].(string)
+	if title, ok := taskUpdates["title"].(string); !ok || strings.TrimSpace(title) == "" {
+		return fmt.Errorf("отсутствует обязательное поле title")
+	}
+
+	if !dateOk || strings.TrimSpace(date) == "" {
+		return fmt.Errorf("отсутствует обязательное поле date")
+	}
+
+	if _, err := time.Parse(service.Format, date); err != nil {
+		return fmt.Errorf("недопустимый формат date")
+	}
+
+	if repeat, ok := taskUpdates["repeat"].(string); ok && strings.TrimSpace(repeat) != "" {
+		repeatParts := strings.SplitN(repeat, " ", 2)
+		repeatType := repeatParts[0]
+		if !isValidRepeatType(repeatType) {
+			return fmt.Errorf("недопустимый символ")
+		}
+	}
+
+	return nil
+}
+
+func (h *Handlers) validateAndUpdateDate(task *entities.Task) error {
+	var dateInTime time.Time
+	var err error
+
+	if task.Date != "" {
+		dateInTime, err = time.Parse(service.Format, task.Date)
+		if err != nil {
+			return fmt.Errorf("недопустимый формат date")
+		}
+	} else {
+		task.Date = time.Now().Format(service.Format)
+		dateInTime = time.Now()
+	}
+
+	if time.Now().After(dateInTime) {
+		if task.Repeat == "" {
+			task.Date = time.Now().Format(service.Format)
+			dateInTime = time.Now()
+		} else {
+			task.Date, err = h.TaskService.NextDate(time.Now(), task.Date, task.Repeat)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (h *Handlers) markTaskAsDone(taskID string, task *entities.Task) error {
+	parsedDate, err := time.Parse(service.Format, task.Date)
+	if err != nil {
+		return fmt.Errorf("недопустимый формат date")
+	}
+
+	if parsedDate.Format(service.Format) == time.Now().Format(service.Format) {
+		parsedDate = parsedDate.AddDate(0, 0, -1)
+		task.Date, err = h.TaskService.NextDate(parsedDate, task.Date, task.Repeat)
+	} else {
+		task.Date, err = h.TaskService.NextDate(time.Now(), task.Date, task.Repeat)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	if err := h.TaskService.Repo.MarkTaskAsDone(taskID, task.Date); err != nil {
+		return fmt.Errorf("ошибка при обновлении задачи")
+	}
+
+	return nil
+}
+
+func (h *Handlers) deleteTaskIfExists(taskID string) error {
+	if _, err := h.TaskService.Repo.GetTaskByID(taskID); err != nil {
+		return fmt.Errorf("задача с указанным id не найдена")
+	}
+
+	if _, err := h.TaskService.Repo.DeleteTask(taskID); err != nil {
+		return fmt.Errorf("ошибка запроса к базе данных")
+	}
+
+	return nil
+}
+
+func isValidRepeatType(repeatType string) bool {
+	validTypes := []string{"d", "w", "m", "y"}
+	for _, v := range validTypes {
+		if repeatType == v {
+			return true
+		}
+	}
+	return false
 }
